a2a1a883b6399f260d68e9f01e3b5d6a7a952bcb
joblakplay <jobson-loiola-cardoso@hotmail.com>
2020-05-18 19:32:36 -0300

    corrigindo routes

--- src/main.js
+++ src/main.js
@@ -3,15 +3,19 @@ import App from './App.vue'
 import 'bootstrap'
 import jQquery from'jquery'
 
+
 window.$ = window.jQquery = jQquery
 
 import 'popper.js'
 import './assets/app.scss'
+
+import router from './router'
 
 //Vue.component('Navbar',require('./components/Navbar.vue').default)
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
