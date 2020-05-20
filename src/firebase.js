48d0d29ff55c22f271f2c8124c65fcd01f1c80c8
joblakplay <jobson-loiola-cardoso@hotmail.com>
2020-05-20 20:14:45 -0300

    adicionou o banco firebase

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+
+import firebase from "firebase"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC7ufVPL912Aki4Mfw8Pu7oOSHp4DbazJ4",
+    authDomain: "vue-shop-4b03c.firebaseapp.com",
+    databaseURL: "https://vue-shop-4b03c.firebaseio.com",
+    projectId: "vue-shop-4b03c",
+    storageBucket: "vue-shop-4b03c.appspot.com",
+    messagingSenderId: "611187509425",
+    appId: "1:611187509425:web:9a62b18ad94ab0acc17a90",
+    measurementId: "G-PFQYXBJFB5"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
